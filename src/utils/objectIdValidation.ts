a66454f140868af95a213ab818936cccb493e543
yonadav <yonadav.barilan@gmail.com>
2022-02-21 20:44:20 +0200

    update

--- src/utils/objectIdValidation.ts
+++ src/utils/objectIdValidation.ts
@@ -12,3 +12,13 @@ export const objectIdValidation = (value: string, helpers: { error: (arg0: strin
 
 export const objectIdOptionalValidation = (value: string | undefined, helpers: { error: (arg0: string) => any; }) =>
   value === undefined ? value : objectIdValidation(value, helpers);
+
+export const isObjectIdValidation = (value: string, helpers: { error: (arg0: string) => any; }) => {
+  if (!isValidObjectId(value)) {
+    return helpers.error(message);
+  }
+  return value;
+};
+
+export const isObjectIdOptionalValidation = (value: string | undefined, helpers: { error: (arg0: string) => any; }) =>
+  value === undefined ? value : isObjectIdValidation(value, helpers);
