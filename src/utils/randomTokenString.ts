08e6fad74fb697937cb2771c340fdff765014463
yonadav <yonadav.barilan@gmail.com>
2022-03-03 13:04:51 +0200

    update

--- src/utils/randomTokenString.ts
+++ src/utils/randomTokenString.ts
@@ -3,3 +3,7 @@ import crypto from "crypto";
 export function randomTokenString() {
   return crypto.randomBytes(40).toString('hex');
 }
+
+export function createToken(size:number=128):string {
+  return crypto.randomBytes(size).toString()
+}
