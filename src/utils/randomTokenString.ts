25d402322bc391ea7ccb6d610cd8bc03448c95f6
yonadav <yonadav.barilan@gmail.com>
2022-03-05 10:15:15 +0200

    update

--- src/utils/randomTokenString.ts
+++ src/utils/randomTokenString.ts
@@ -4,6 +4,11 @@ export function randomTokenString() {
   return crypto.randomBytes(40).toString('hex');
 }
 
-export function createToken(size:number=128):string {
-  return crypto.randomBytes(size).toString()
+export type CreateToken = (params?:{
+  size?: number,
+  encoding?: 'ascii'|'utf8'|'ucs2'|'base64'|'binary'|'hex',
+}) => string;
+
+export const createToken:CreateToken = ({ size=128, encoding='hex' }={size:128, encoding:'hex'}) => {
+  return crypto.randomBytes(size).toString(encoding)
 }
